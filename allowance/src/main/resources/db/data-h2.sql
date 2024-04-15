7ec4352f069252aae52d9758008452e49248c27e
IMWoo94 <leemeawoo94@gmail.com>
2024-04-15 22:38:37 +0900

    feat : Mission table init
    1. Mission schema create
    2. Mission init data insert

--- allowance/src/main/resources/db/data-h2.sql
+++ allowance/src/main/resources/db/data-h2.sql
@@ -1,4 +1,19 @@
-INSERT INTO Sample (description) values ('베베숲 물티슈');
-INSERT INTO Sample (description) values ('여름 토퍼');
-INSERT INTO Sample (description) values ('페이크 삭스');
-INSERT INTO Sample (description) values ('우산');
+-- Sample data
+INSERT INTO Sample (description)
+values ('베베숲 물티슈');
+INSERT INTO Sample (description)
+values ('여름 토퍼');
+INSERT INTO Sample (description)
+values ('페이크 삭스');
+INSERT INTO Sample (description)
+values ('우산');
+
+-- Mission data
+INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
+values ('혜택 좋은 신용카드', 2, '2024-04-01', '2024-06-01');
+INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
+values ('Color Blocks 3D(레벨 151 클리어)', 10, '2024-04-01', '2024-06-01');
+INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
+values ('무신사', 2, '2024-03-01', '2024-04-01');
+INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
+values ('올리브영', 2, '2024-03-01', '2024-04-01');
--- allowance/src/main/resources/db/schema-h2.sql
+++ allowance/src/main/resources/db/schema-h2.sql
@@ -1,6 +1,22 @@
+-- Sample Table
 DROP TABLE IF EXISTS Sample;
 create table Sample
 (
     id          INTEGER auto_increment,
     description varchar2 not null
 );
+
+-- Mission Table
+DROP TABLE IF EXISTS Mission;
+create table Mission
+(
+    mission_id     LONG auto_increment,
+    mission_name   VARCHAR2 not null,
+    mission_amount INT      not null,
+    start_date     DATE     not null,
+    end_date       DATE     not null,
+    active         TINYINT  not null default 1,
+    constraint Mission_pk
+        primary key (mission_id)
+);
+
