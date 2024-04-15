6206be22aecefbeba5d631b1a897ac20aeae3e69
IMWoo94 <leemeawoo94@gmail.com>
2024-04-15 20:34:00 +0900

    feat : swagger, objectmapper configuration 추가
    1. Object Mapper Json 옵션 설정
    - java 8 버전 이후 클래스
    - Json 매핑 시 Unknown 무시
    - 비어있는 경우 무시 등
    2. 설정 ObjectMapper Swagger 에 적용

---
+++ allowance/src/main/java/com/daily/allowance/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package com.daily.allowance.config.swagger;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.core.jackson.ModelResolver;
+
+@Configuration
+@ConditionalOnBean(ObjectMapper.class)
+public class SwaggerConfiguration {
+
+	@Bean
+	public ModelResolver modelResolver (ObjectMapper objectMapper){
+		return new ModelResolver(objectMapper);
+	}
+}
