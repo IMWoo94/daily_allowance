1a5960a4eccd4f08728d221d6b2e4a333d16f2e6
IMWoo94 <leemeawoo94@gmail.com>
2024-04-15 20:31:23 +0900

    feat : h2 Database, MyBatis 적용
    1. h2 Database Embedded 적용
    - h2.mv.db
    2. MyBatis mapper.xml 경로 수정
    3. 초기화 스크립트 반영
    - schema-h2.sql
    - data-h2.sql

---
+++ allowance/src/main/resources/db/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO Sample (description) values ('베베숲 물티슈');
+INSERT INTO Sample (description) values ('여름 토퍼');
+INSERT INTO Sample (description) values ('페이크 삭스');
+INSERT INTO Sample (description) values ('우산');
---
+++ allowance/src/main/resources/db/schema-h2.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS Sample;
+create table Sample
+(
+    id          INTEGER auto_increment,
+    description varchar2 not null
+);
