5b1ad4eec479f2c8cdf1b81e13e7ebffb1872b9f
IMWoo94 <leemeawoo94@gmail.com>
2024-04-16 18:37:26 +0900

    feat : DB data schema, data 변경
    1. 지급 내역을 관리하는 테이블 생성
    2. 사용자 정보를 담을 간단한 Member 테이블 생성
    3. 초기 데이터 추가

--- allowance/src/main/resources/db/data-h2.sql
+++ allowance/src/main/resources/db/data-h2.sql
@@ -8,6 +8,26 @@ values ('페이크 삭스');
 INSERT INTO Sample (description)
 values ('우산');
 
+-- Users data
+INSERT INTO Member (member_name)
+values ('라이언');
+INSERT INTO Member (member_name)
+values ('춘식이');
+INSERT INTO Member (member_name)
+values ('어피치');
+INSERT INTO Member (member_name)
+values ('프로도');
+INSERT INTO Member (member_name)
+values ('제이지');
+INSERT INTO Member (member_name)
+values ('무지');
+INSERT INTO Member (member_name)
+values ('튜브');
+INSERT INTO Member (member_name)
+values ('네오');
+INSERT INTO Member (member_name)
+values ('콘');
+
 -- Mission data
 INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
 values ('혜택 좋은 신용카드', 2, '2024-04-01', '2024-06-01');
@@ -17,3 +37,17 @@ INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
 values ('무신사', 2, '2024-03-01', '2024-04-01');
 INSERT INTO Mission (mission_name, mission_amount, start_date, end_date)
 values ('올리브영', 2, '2024-03-01', '2024-04-01');
+
+-- Payment data
+-- 미션
+INSERT INTO Payment (payment_amount, payment_date, status, member_id, mission_id)
+values (2, '2024-04-01', 'ok', 1, 1);
+INSERT INTO Payment (payment_amount, payment_date, status, member_id, mission_id)
+values (2, '2024-04-02', 'ok', 1, 1);
+INSERT INTO Payment (payment_amount, payment_date, status, member_id, mission_id)
+values (2, '2024-04-03', 'ok', 1, 1);
+INSERT INTO Payment (payment_amount, payment_date, status, member_id, mission_id)
+values (10, '2024-04-03', 'ok', 1, 2);
+-- 데일리 용돈
+INSERT INTO Payment (payment_amount, payment_date, status, member_id)
+values (3, '2024-04-03', 'ok', 1);
--- allowance/src/main/resources/db/schema-h2.sql
+++ allowance/src/main/resources/db/schema-h2.sql
@@ -1,3 +1,5 @@
+-- CASCADE 옵션의 경우 전체 테이블의 완전 초기화를 목적으로 사용
+-- FK 제약 조건 등이 테이블 삭제에 영향을 끼치므로 적용하였으나, 실무에서는 사용 X
 -- Sample Table
 DROP TABLE IF EXISTS Sample;
 create table Sample
@@ -6,8 +8,18 @@ create table Sample
     description varchar2 not null
 );
 
+-- User Table
+DROP TABLE IF EXISTS Member CASCADE;
+create table Member
+(
+    member_id          LONG auto_increment,
+    member_name        varchar2 not null,
+    constraint Member_pk
+        primary key (member_id)
+);
+
 -- Mission Table
-DROP TABLE IF EXISTS Mission;
+DROP TABLE IF EXISTS Mission CASCADE;
 create table Mission
 (
     mission_id     LONG auto_increment,
@@ -20,3 +32,20 @@ create table Mission
         primary key (mission_id)
 );
 
+-- Payment Table
+DROP TABLE IF EXISTS Payment;
+create table Payment
+(
+    payment_id     LONG auto_increment,
+    payment_amount INT      not null,
+    payment_date   DATE     not null,
+    status         VARCHAR2 not null,
+    member_id      LONG     not null,
+    mission_id     LONG,
+    constraint Payment_pk
+        primary key (payment_id),
+    constraint Payment_Member_Member_id_fk
+        foreign key (member_id) references Member,
+    constraint Payment_Mission_Mission_id_fk
+        foreign key (mission_id) references Mission
+)
